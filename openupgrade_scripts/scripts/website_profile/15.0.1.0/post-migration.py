e1e49c95696675ea2d79b932555974c4437622d8
sang250399 <sangfpt1999alhp@gmail.com>
2022-05-11 08:40:44 +0700

    [MIG]website_slides_survey: migration on 15.0

--- openupgrade_scripts/scripts/website_profile/15.0.1.0/post-migration.py
+++ openupgrade_scripts/scripts/website_profile/15.0.1.0/post-migration.py
@@ -5,3 +5,10 @@ from openupgradelib import openupgrade
 def migrate(env, version):
     # Load noupdate changes
     openupgrade.load_data(env.cr, "website_profile", "15.0.1.0/noupdate_changes.xml")
+    openupgrade.delete_record_translations(
+        env.cr,
+        "website_profile",
+        [
+            "validation_email",
+        ],
+    )
