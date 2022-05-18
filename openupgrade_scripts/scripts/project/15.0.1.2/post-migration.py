7c3b5fcfd67717120be2d9726cc63dfcb06d331a
Roy Le <roy.le@viindoo.com>
2022-05-18 11:02:23 +0700

    imp

--- openupgrade_scripts/scripts/project/15.0.1.2/post-migration.py
+++ openupgrade_scripts/scripts/project/15.0.1.2/post-migration.py
@@ -8,46 +8,63 @@ def _convert_field_m2o_to_m2m(env):
     )
 
 
-def _migration_add_followes_allowed_internal_user_ids_project(env):
+def _add_followes_allowed_internal_user_to_project_project(env):
     openupgrade.logged_query(
         env.cr,
         """
-            SELECT project_project_id as pid, res_users_id as uid
-            FROM project_allowed_internal_users_rel
+        INSERT INTO mail_followers (res_model, res_id, partner_id)
+        SELECT 'project.project', prj_user_rel.project_project_id, users.partner_id
+        FROM project_allowed_internal_users_rel prj_user_rel
+        JOIN res_users users ON users.id = prj_user_rel.res_users_id
+        JOIN project_project prj
+            ON prj.id = prj_user_rel.project_project_id
+            AND prj.privacy_visibility = 'followers'
+        ON CONFLICT (res_model, res_id, partner_id) DO NOTHING
         """,
     )
-    for r in env.cr.dictfetchall():
-        projects = env["project.project"].browse(r["pid"]).exists()
-        partner_ids = env["res.users"].browse(r["uid"]).exists().partner_id.ids
-        projects.message_subscribe(partner_ids)
 
 
-def _migration_add_followes_allowed_portal_user_ids_project(env):
+def _add_followes_allowed_portal_user_to_project_project(env):
     openupgrade.logged_query(
         env.cr,
         """
-            SELECT project_project_id as pid, res_users_id as uid
-            FROM project_allowed_portal_users_rel
+        INSERT INTO mail_followers (res_model, res_id, partner_id)
+        SELECT 'project.project', prj_user_rel.project_project_id, users.partner_id
+        FROM project_allowed_portal_users_rel prj_user_rel
+        JOIN res_users users ON users.id = prj_user_rel.res_users_id
+        JOIN project_project prj
+            ON prj.id = prj_user_rel.project_project_id
+            AND prj.privacy_visibility = 'portal'
+        ON CONFLICT (res_model, res_id, partner_id) DO NOTHING
         """,
     )
-    for r in env.cr.dictfetchall():
-        projects = env["project.project"].browse(r["pid"]).exists()
-        partner_ids = env["res.users"].browse(r["uid"]).exists().partner_id.ids
-        projects.message_subscribe(partner_ids)
 
 
-def _migration_add_followes_allowed_user_ids_task(env):
+def _add_followes_allowed_user_to_project_task(env):
     openupgrade.logged_query(
         env.cr,
         """
-            SELECT project_task_id as tid, res_users_id as uid
-            FROM project_task_res_users_rel
+        INSERT INTO mail_followers (res_model, res_id, partner_id)
+        SELECT 'project.task', task_user_rel.project_task_id, users.partner_id
+        FROM project_task_res_users_rel task_user_rel
+        JOIN res_users users ON users.id = task_user_rel.res_users_id
+        ON CONFLICT (res_model, res_id, partner_id) DO NOTHING
         """,
     )
-    for r in env.cr.dictfetchall():
-        tasks = env["project.task"].browse(r["tid"]).exists()
-        partner_ids = env["res.users"].browse(r["uid"]).exists().partner_id.ids
-        tasks.message_subscribe(partner_ids)
+
+
+def _fill_project_task_display_project_id(env):
+    # Example: 1 task A with 1 subtask B in project P
+    # A -> project_id=P, display_project_id=P
+    # B -> project_id=P (to inherit from ACL/security rules), display_project_id=False
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE project_task task
+        SET display_project_id = task.project_id
+        WHERE task.parent_id IS NULL
+        """,
+    )
 
 
 @openupgrade.migrate()
@@ -63,6 +80,7 @@ def migrate(env, version):
         ],
     )
     _convert_field_m2o_to_m2m(env)
-    _migration_add_followes_allowed_internal_user_ids_project(env)
-    _migration_add_followes_allowed_portal_user_ids_project(env)
-    _migration_add_followes_allowed_user_ids_task(env)
+    _add_followes_allowed_internal_user_to_project_project(env)
+    _add_followes_allowed_portal_user_to_project_project(env)
+    _add_followes_allowed_user_to_project_task(env)
+    _fill_project_task_display_project_id(env)
