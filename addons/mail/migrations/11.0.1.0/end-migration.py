95dfb4dd29fe932e925e58ee9d891529d44a6c52
Dang Thanh Luan <65998903+thanhluan240287@users.noreply.github.com>
2022-06-03 10:26:22 +0700

    [IMP]mail: improve end-migration

--- addons/mail/migrations/11.0.1.0/end-migration.py
+++ addons/mail/migrations/11.0.1.0/end-migration.py
@@ -1,18 +1,17 @@
 from openupgradelib import openupgrade
 
-_removed_model_list = [
-    'procurement.order'
-    ]
 
-def delete_mail_message_with_model_removed(env):
-    for model in _removed_model_list:
-        if not env.__contains__(model):
-            openupgrade.logged_query(
-                env.cr, """
-                DELETE FROM mail_message
-                WHERE model = {};""".format(model))
+def unlink_deprecated_message_from_channel(env):
+    openupgrade.logged_query(
+        env.cr,
+        """DELETE FROM mail_message_mail_channel_rel
+           WHERE mail_message_id in (
+               SELECT mm.id FROM mail_message_mail_channel_rel mmmcr
+               JOIN mail_message mm ON mmmcr.mail_message_id = mm.id
+               AND mm.model IS not null
+               AND mm.model not in %s)""", (tuple(env.registry.models),))
 
 
 @openupgrade.migrate()
 def migrate(env, version):
-    delete_mail_message_with_model_removed(env)
+    unlink_deprecated_message_from_channel(env)
