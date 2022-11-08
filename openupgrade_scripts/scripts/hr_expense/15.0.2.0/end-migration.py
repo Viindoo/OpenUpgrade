81a73d0e5472d81b710ccc45399307edece9f2bc
jocelyn <jocelyn@system76>
2022-11-08 13:57:05 +0700

    [MIG] hr_expense: add migration for field unit_amount

--- openupgrade_scripts/scripts/hr_expense/15.0.2.0/end-migration.py
+++ openupgrade_scripts/scripts/hr_expense/15.0.2.0/end-migration.py
@@ -6,6 +6,27 @@ def recompute_total_amount_company(env):
     expense_to_recompute._compute_total_amount_company()
 
 
+def update_expense_unit_amount(env):
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE hr_expense
+        SET unit_amount = 0.0
+        WHERE id IN (
+            SELECT he.id
+            FROM hr_expense he
+            JOIN product_product pp ON he.product_id = pp.id
+            LEFT JOIN ir_property ip ON ip.name = 'standard_price'
+                AND ip.res_id = CONCAT('product.product,', pp.id)
+                AND he.company_id = ip.company_id
+            WHERE  he.unit_amount != 0.0
+                AND ip.value_float IS NULL
+        )
+        """,
+    )
+
+
 @openupgrade.migrate()
 def migrate(env, version):
+    update_expense_unit_amount(env)
     recompute_total_amount_company(env)
