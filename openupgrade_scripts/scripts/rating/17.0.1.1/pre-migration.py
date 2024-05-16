979534f936895b842184a64a4337933ee6aa8047
haumenphai <codegunpow@gmail.com>
2024-05-16 13:39:51 +0700

    [MIG] rating: migrate to 17.0

---
+++ openupgrade_scripts/scripts/rating/17.0.1.1/pre-migration.py
@@ -0,0 +1,6 @@
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.delete_sql_constraint_safely(env, "rating", "rating_rating", "rating_range")
