1763bceec1a18feeccae34d1399c42520ae54af0
Giang Pham <phamgiang2510@gmail.com>
2025-12-08 15:07:33 +0700

    [FIX] hr_holidays: Data sould be load after field conversion

--- openupgrade_scripts/scripts/hr_holidays/17.0.1.6/post-migration.py
+++ openupgrade_scripts/scripts/hr_holidays/17.0.1.6/post-migration.py
@@ -32,9 +32,9 @@ def _compute_already_accrued(env):
 
 @openupgrade.migrate()
 def migrate(env, version):
-    openupgrade.load_data(env, "hr_holidays", "17.0.1.6/noupdate_changes.xml")
     _leave_type_responsible_convert_field_m2o_to_m2m(env)
     _compute_already_accrued(env)
+    openupgrade.load_data(env, "hr_holidays", "17.0.1.6/noupdate_changes.xml")
     openupgrade.delete_records_safely_by_xml_id(
         env,
         [
